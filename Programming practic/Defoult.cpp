fcaca18ee25739f5b1f47e76e7a11d1c80702519
Степанюк Андрей <Xmury.ya@yandex.ru>
2017-11-30 10:12:29 +0600

    This is round road

--- Programming practic/Defoult.cpp
+++ Programming practic/Defoult.cpp
@@ -30,33 +30,6 @@ void hard_task(){
     cout << "Pass" << endl;
 }
 int main(){
-    int a; cout << "Change task: 1, 2, 3, 4; add(5, 6); hard(7): "; cin >> a;
 
-switch(a){
-        case 1:
-            task_1(); break;
-        
-        case 2:
-            task_2(); break;
-        
-        case 3:
-            task_3(); break;
-        
-        case 4:
-            task_4(); break;
-
-        case 5:
-            add_task_1(); break;
-        
-        case 6:
-            add_task_2(); break;
-
-        case 7:
-            hard_task(); break;
-
-        default:
-            cout << "Your input are wrong" << endl;
-    }
-    
     return 0;
 }
