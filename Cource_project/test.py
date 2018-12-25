c3e0a6abdb26a203b50d97d8348d5b7b454d1a03
xmury <xmury.ya@gmail.com>
2018-12-25 21:54:46 +0600

    new GUI and add serial driver

--- Cource_project/test.py
+++ Cource_project/test.py
@@ -1,42 +1,76 @@
 import tkinter as tk
 import math
+from serial_driver import *
 
 class Application(tk.Frame):
     time = 0
     timer_on = False
-
     
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
-        self.createWidgets()
+        self.createTablo()
+        self.createPanel()
 
-
-
-
-    def createWidgets(self):
+    def createTablo(self):
         self.now = tk.StringVar()
         self.time_b = tk.Button(self, text = '00:00', command=self.timer_go)
-        self.time_b.grid(column=2, row=0)
         self.time_b["textvariable"] = self.now
         self.now.set('00:00')
         self.timer()
 
-        self.b1 = tk.Button(self ,  text='счёт1')
-        self.b2 = tk.Button(self ,  text='общ1')
-        self.arrow1 = tk.Button(self ,  text='стрелка1')
-        self.arrow2 = tk.Button(self ,  text='стрелка2')
-        self.b3 = tk.Button(self ,  text='счёт2')
-        self.b4 = tk.Button(self ,  text='общ2')
-        self.b5 = tk.Button(self ,  text='раунд')
+        self.load =     tk.Button(self , text='Загрузить игру')
+        self.save =     tk.Button(self , text='Сохранить игру')
+        self.road =     tk.Button(self , text='Период: 0')
+        self.score1 =   tk.Button(self , text='0')
+        self.score2 =   tk.Button(self , text='0')
+        self.arrow1 =   tk.Button(self , text='>')
+        self.arrow2 =   tk.Button(self , text='<')
+
+        self.time_b.grid(column=3 , row=2, columnspan=3)
         
-        self.b1.grid(column=0 , padx=5 , pady=5 , row=0)
-        self.b2.grid(column=1 , padx=5 , pady=5 , row=0)
-        self.b3.grid(column=3 , padx=5 , pady=5 , row=0)
-        self.b4.grid(column=4 , padx=5 , pady=5 , row=0)
-        self.b5.grid(column=2 , padx=5 , pady=5 , row=1)
-        self.arrow1.grid(columnspan=2, column=0 , padx=5 , pady=5 , row=1)
-        self.arrow2.grid(columnspan=2,column=3 , padx=5 , pady=5 , row=1)
+        self.load.grid(  column=0 , row=0, columnspan=2, padx=5 , pady=5)
+        self.save.grid(  column=7 , row=0, columnspan=2, padx=5 , pady=5)
+        self.road.grid(  column=4 , row=0,               padx=5 , pady=5)
+        self.score1.grid(column=1 , row=2,               padx=5 , pady=5)
+        self.score2.grid(column=7 , row=2,               padx=5 , pady=5)
+        self.arrow1.grid(column=2 , row=2)
+        self.arrow2.grid(column=6 , row=2)
+
+    def createPanel(self):
+        # Для команды 1
+        self.shadow1 = tk.Label(self, text='')
+        self.shadow2 = tk.Label(self, text='')
+        self.shadow1.grid(column=0 , row=1)
+        self.shadow2.grid(column=0 , row=3)
+
+        self.add11 = tk.Button(self , text='+1')
+        self.add12 = tk.Button(self , text='+2')
+        self.add13 = tk.Button(self , text='+3')
+        self.min1  = tk.Button(self , text='-1')
+
+        self.add11.grid(column=0 , row=4, padx=5 , pady=5)
+        self.add12.grid(column=1 , row=4, padx=5 , pady=5)
+        self.add13.grid(column=2 , row=4, padx=5 , pady=5)
+        self.min1.grid( column=0 , row=5, columnspan=3, padx=5 , pady=5)
+        # Для команды 2
+        self.add21 = tk.Button(self , text='+1')
+        self.add22 = tk.Button(self , text='+2')
+        self.add23 = tk.Button(self , text='+3')
+        self.min2  = tk.Button(self , text='-1')
+
+        self.add21.grid(column=6 , row=4, padx=5 , pady=5)
+        self.add22.grid(column=7 , row=4, padx=5 , pady=5)
+        self.add23.grid(column=8 , row=4, padx=5 , pady=5)
+        self.min2.grid( column=6 , row=5, columnspan=3, padx=5 , pady=5)
+
+        # Старт и пауза
+        self.start  = tk.Button(self , text='Старт')
+        self.pause  = tk.Button(self , text='Пауза')
+
+        self.start.grid(column=4 , row=4, padx=5 , pady=5)
+        self.pause.grid(column=4 , row=5, padx=5 , pady=5)
+
 
     def timer_go(self):
         if self.timer_on:
@@ -64,58 +98,9 @@ class Application(tk.Frame):
             self.now.set(time_f)
         self.after(1000, self.timer)
 
-class Application2(tk.Frame):
-    time = 0
-    timer_on = False
-
-    
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-
-    def createWidgets(self):
-        self.vol = tk.Button(self ,  text='Волейбол')
-        self.bas = tk.Button(self ,  text='Баскетбол')
-        self.fut = tk.Button(self ,  text='Футбол')
-        self.back = tk.Button(self ,  text='Отменить')
-        
-        self.vol.grid(column=1 , padx=5 , pady=5 , row=0)
-        self.bas.grid(column=2 , padx=5 , pady=5 , row=0)
-        self.fut.grid(column=3 , padx=5 , pady=5 , row=0)
-        self.back.grid(column=2 , padx=5 , pady=5 , row=4)
-        
-        i = 1; 
-        self.key = [0,0,0,0,0,0,0,0,0,0]
-        for cn in range(1,4):
-            for rw in range(1,4):
-                self.key[i] = tk.Button(self ,  text=i)
-                self.key[i].grid(column=cn , padx=5 , pady=5 , row=rw)
-                i += 1           
-
-        self.fine1 = tk.Button(self ,  text='Штраф')
-        self.fine2 = tk.Button(self ,  text='Штраф')
-
-        self.fine1.grid(column=0 , padx=5 , pady=5 , row=3)
-        self.fine2.grid(column=4 , padx=5 , pady=5 , row=3)
-
-        self.prize1 = tk.Button(self ,  text='Добавить очки')
-        self.prize2 = tk.Button(self ,  text='Добавить очки')
-
-        self.prize1.grid(column=0 , padx=5 , pady=5 , row=4)
-        self.prize2.grid(column=4 , padx=5 , pady=5 , row=4)
-
-
 root = tk.Tk()
 root.geometry()
 app = Application(master=root)
-app2 = Application2(master=root)
 
 root.grid_columnconfigure((0, 1, 2), weight=1)
-
-
-
-
-# app2 = Application(master=root)
-
 root.mainloop()
